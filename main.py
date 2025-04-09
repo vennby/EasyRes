6cebee34a1d93b32579fb7c7f564929aa1ab3ac5
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2025-04-09 14:39:11 +0000

    today's work

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Yello! This is Phil!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
