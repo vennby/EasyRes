61a22e3840f199bdc86c83d80fefc9f2bc81571f
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2025-07-01 17:39:01 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -33,7 +33,7 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('instance/' + DB_NAME):
         with app.app_context():
             db.create_all()
             print('Created Database!')
